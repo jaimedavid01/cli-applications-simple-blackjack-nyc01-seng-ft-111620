45beebb68be6629f31d5a280695d64326ac6dfe0
Jaime David <jaimedavid01@yahoo.com>
2020-11-12 03:29:30 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -1,17 +1,23 @@
 def welcome
   # code #welcome here
+  puts "Welcome to the Blackjack Table"
+  
 end
 
 def deal_card
   # code #deal_card here
+  rand(1..11)
+  
 end
 
-def display_card_total
+def display_card_total(card_total)
   # code #display_card_total here
+  puts "Your cards add up to #{card_total}"
 end
 
 def prompt_user
   # code #prompt_user here
+  puts 
 end
 
 def get_user_input
